eb6d284311d1fffccf3d405c15e624a3da535fa4
Silly Duck <itsmekshka@gmail.com>
2026-04-08 16:10:52 +0530

    Added features

--- views/contact.py
+++ views/contact.py
@@ -1,3 +1,4 @@
 import streamlit as st
 
 st.title("Contact")
+st.write('<p style="font-size:25px;">"No Need"</p>', unsafe_allow_html=True)
--- views/service.py
+++ views/service.py
@@ -1,3 +1,9 @@
 import streamlit as st
 
 st.title("Service")
+st.write('<p style="font-size:25px;">"Not Needed"</p>', unsafe_allow_html=True)
+
+picture = st.camera_input("Take a pic")
+
+if picture:
+    st.image(picture)
